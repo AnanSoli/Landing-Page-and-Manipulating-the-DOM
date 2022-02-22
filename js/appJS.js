23042ee15ebc263992f0042c120a395a9a5e6154
Anan Ahmed <86473646+AnanSoli@users.noreply.github.com>
2022-02-22 15:00:44 +0200

    Update appJS.js

--- js/appJS.js
+++ js/appJS.js
@@ -93,7 +93,7 @@ document.querySelectorAll('a.links').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
     document.querySelector(this.getAttribute("href")).scrollIntoView({
-      {behavior: "smooth"}
+      behavior: "smooth",block: "end", inline: "nearest"
     });
   });
 });
